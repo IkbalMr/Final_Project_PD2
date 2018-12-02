555ebbb95e6e8dc3353eb53b0e64f7c9b12e629c
IkbalMr <root.lastc0de@gmail.com>
2018-12-02 10:07:52 +0700

    belum fix

---
+++ src/ta/Mahasiswa.java
@@ -0,0 +1,60 @@
+package ta;
+
+
+public class Mahasiswa {
+    
+    private int nim;
+    private String nama;
+    private String alamat;
+    private String tgl_lahir;
+    private String jk;
+    private String jurusan;
+    private String email;
+    
+    public Mahasiswa(int NIM, String Nama, String Alamat, String TanggalLahir, String JenisKelamin, String Jurusan, String Email)
+    {
+        this.nim = NIM;
+        this.nama = Nama;
+        this.alamat = Alamat;
+        this.tgl_lahir = TanggalLahir;
+        this.jk = JenisKelamin;
+        this.jurusan = Jurusan;
+        this.email = Email;
+        
+    }
+    
+    public int getNIM()
+    {
+        return nim;
+    }
+    
+    public String getNama()
+    {
+        return nama;
+    }
+    
+    public String getAlamat()
+    {
+        return alamat;
+    }
+    
+    public String getTglLhr()
+    {
+        return tgl_lahir;
+    }
+    
+    public String getJK()
+    {
+        return jk;
+    }
+    
+    public String getJurusan()
+    {
+        return jurusan;
+    }
+    
+    public String getEmail()
+    {
+        return email;
+    }
+}
